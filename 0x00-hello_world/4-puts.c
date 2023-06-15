c1ce3898989e6a104c645b2860df19abcf25b8df
mahmoudabouzaid <mahmoudabouzeid255@gmail.com>
2023-06-15 03:54:27 -0700

    updat

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Alawys 0 (Success)
- */
+* main - Entry point
+*
+* Return: Alawys 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
