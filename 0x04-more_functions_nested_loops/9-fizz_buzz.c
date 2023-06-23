2a7620608db70d657392b772ac74f381fe09d264
mahmoudabouzaid <mahmoudabouzeid255@gmail.com>
2023-06-23 06:11:46 -0700

    updat

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,20 +13,27 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && !(i % 5 == 0))
-			printf("Fizz");
-		else if (i % 5 == 0 && !(i % 3 == 0))
-			printf("Buzz");
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
 		else if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
 			printf("%d", i);
-
-		if (i != 100)
-			printf(" ");
+		}
 		else
-			printf("\n");
+		{
+			printf(" %d", i);
+		}
 	}
-
+	printf("\n");
 	return (0);
 }
