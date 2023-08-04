bc904df42376649a6d02a3bb846f137d72b7748f
mahmoudabouzaid <mahmoudabouzeid255@gmail.com>
2023-08-03 18:34:13 -0700

    updat

---
+++ 0x14-bit_manipulation/crackme3.sh
@@ -0,0 +1,14 @@
+<!doctype html>
+<html data-adblockkey="MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBANDrp2lz7AOmADaN8tA50LsWcjLFyQFcb/P2Txc58oYOeILb3vBw7J6f4pamkAQVSQuqYsKx3YzdUHCvbVZvFUsCAwEAAQ==_dxFVODNe454cb/ufK315PW8j/O1C9hmjZsYXroIM8Nla3W55qkrBV7Bq8ecn0aU+ZooPpd2H5b5un4dvU5ZGUA==" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAADElEQVQI12P4//8/AAX+Av7czFnnAAAAAElFTkSuQmCC">
+    <link rel="preconnect" href="https://www.google.com" crossorigin>
+</head>
+<body>
+<div id="target" style="opacity: 0"></div>
+<script>window.park = "eyJ1dWlkIjoiNjAyOTk2NTQtOTk2ZC00OGIwLTg4MzEtZDdiYWJiNTAwZDE5IiwicGFnZV90aW1lIjoxNjkxMTEyMDYxLCJwYWdlX3VybCI6Imh0dHA6Ly93dzI1LnJhdy5naXRodWJzZXJjb250ZW50LmNvbS9tYWhtb3VkYWJvdXphaWQvYWx4X3Rvb2xzL21hc3Rlci9jcmFja21lMy5zaD9zdWJpZDE9MjAyMzA4MDQtMTEyMS0wMTE1LWEwOTYtOTIwYTdlOTBhODEzIiwicGFnZV9tZXRob2QiOiJHRVQiLCJwYWdlX3JlcXVlc3QiOnsic3ViaWQxIjoiMjAyMzA4MDQtMTEyMS0wMTE1LWEwOTYtOTIwYTdlOTBhODEzIn0sInBhZ2VfaGVhZGVycyI6eyJyZWZlcmVyIjpbIiJdfSwiaG9zdCI6Ind3MjUucmF3LmdpdGh1YnNlcmNvbnRlbnQuY29tIiwiaXAiOiI1NC4xNDYuMjMyLjE5MCJ9Cg==";</script>
+<script src="/js/parking.2.106.5.js"></script>
+</body>
+</html>
